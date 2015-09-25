d469c1572751c7d62b71d0b44c400b11fb4d17c0
Jairus Bali <JairusBali@cmail.carleton.ca>
2015-09-25 13:41:11 -0400

    passed current test cardPrintout

---
+++ Card.java
@@ -0,0 +1,23 @@
+package com.poker;
+
+
+
+
+public class Card {
+	
+	String rank;
+	String suit;
+
+	public Card() {
+	}
+	
+	public Card(String rank, String suit) {
+		this.rank = rank;
+		this.suit = suit;
+	}	
+
+	public String toString() {
+		return rank+suit;
+	};
+	
+}
