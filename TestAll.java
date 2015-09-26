36a3af3db8f1aeb278171785c80bf4cc9e8f1baf
Jairus Bali <JairusBali@cmail.carleton.ca>
2015-09-26 13:32:45 -0400

    created a TestAll class, to run all of the tests from one point

---
+++ TestAll.java
@@ -0,0 +1,17 @@
+package com.poker;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	TestPoker.class,
+	TestPokerOutcomes.class
+})
+
+
+
+public class TestAll {
+
+}
